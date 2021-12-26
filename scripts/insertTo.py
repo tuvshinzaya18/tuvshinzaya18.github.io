fa50bc44e48464233907a3fec3b79286160cb266
Tuvshinzaya Erdenekhuu <tuvshinzaya.erdenekhuu@student.manchester.ac.uk>
2021-12-26 20:33:18 +0000

    everything working (kinda)

--- scripts/insertTo.py
+++ scripts/insertTo.py
@@ -1,11 +1,11 @@
-def insertToText(source: str,insert: str) -> str:
+def insertToTextFile(entryPoint: str,source: str,insert: str) -> str:
     start:str = ""
     end:str = ""
     mid:str = ""
     with open(source) as file:
         lines: list[str] = file.readlines()
         i:int = 0
-        while "{{insert}}" not in lines[i]:
+        while "{{" + entryPoint + "}}" not in lines[i]:
             i = i + 1
             if i >= len(lines):
                 break
@@ -19,3 +19,23 @@ def insertToText(source: str,insert: str) -> str:
     with open(insert) as file:
         mid = file.read()
     return start + mid + '\n' + end
+
+def insertToTextString(entryPoint: str,source: str,insert: str) -> str:
+    start:str = ""
+    end:str = ""
+    mid:str = ""
+    lines: list[str] = source.split("\n")
+    i:int = 0
+    while "{{" + entryPoint + "}}" not in lines[i]:
+        i = i + 1
+        if i >= len(lines):
+            break
+    if i == len(lines):
+        return "no place to insert"
+    start = '\n'.join(lines[:i])
+    if i >= len(lines) - 1:
+        end = ""
+    else:
+        end = '\n'.join(lines[i+1:])
+    mid = insert
+    return start + '\n' + mid + '\n' + end
--- scripts/process.py
+++ scripts/process.py
@@ -1,9 +1,15 @@
-from insertTo import insertToText
+from insertTo import insertToTextFile,insertToTextString
 from typing import List 
 # Names of pages
 pages:List[str] = ["index","more","projects","resume","books"]
 
-html:str = insertToText("../template/master.html","../template/index.html")
-
+html:str = insertToTextFile("insert","../template/master.html","../template/index.html")
+html:str = insertToTextString("style",html,"<link rel=\"stylesheet\" type=\"text/css\" href=\"../styles/index.css\">")
 with open("result.html","w") as file:
     file.write(html)
+
+for name in pages:
+    html:str = insertToTextFile("insert","../template/master.html","../template/"+name+".html")
+    html:str = insertToTextString("style",html,"<link rel=\"stylesheet\" type=\"text/css\" href=\"../styles/"+name+".css\">")
+    with open("../output/"+name+".html","w") as file:
+        file.write(html)
